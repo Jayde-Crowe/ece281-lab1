9250dff98f622dbb310a5a245a7ac45705bd18ee
jayde-crowe <@users.noreply.github.com>
2024-03-11 16:44:38 -0600

    forgot a file

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -104,8 +104,7 @@ begin
 	
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.
 	
-	MUX_8T1_inst: MUX_8T1
-        port map( 
+	MUX_8T1_inst : MUX_8T1 port map( 
         SEL => w_sel,
         D_IN(0) => i_D,
         D_IN(1) => i_D,
